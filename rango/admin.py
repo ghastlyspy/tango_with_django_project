1f15aca439d8577281ccf688b65ae38c9c528816
Sufiyyan Panezai <sufiyyanpanezai21@outlook.com>
2022-02-09 19:24:18 +0500

    Chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -4,12 +4,14 @@ from rango.models import Category, Page
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'views', 'likes']
+    prepopulated_fields = {'slug': ('name',)}
 
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'url']
     list_filter = ['category']
     list_editable = ['category']
+    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Category, CategoryAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
@@ -18,7 +24,14 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Page, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = 'pages'
 
     def __str__(self):
         return self.title
-
